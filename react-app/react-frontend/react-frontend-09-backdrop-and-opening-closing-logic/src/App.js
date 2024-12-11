cfef9c1a91d002da0ae94de4fa5c3db0c5c52fe5
karthik.yelisetti <karthikyelisetti.dev@gmail.com>
2024-12-11 08:43:29 +0530

    Added Backdrop concept using portal feature to the react app

---
+++ react-app/react-frontend/react-frontend-09-backdrop-and-opening-closing-logic/src/App.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate
+} from 'react-router-dom';
+
+import Users from './user/pages/Users';
+import NewPlace from './places/pages/NewPlace';
+import MainNavigation from './shared/components/Navigation/MainNavigation';
+
+const App = () => {
+  return (
+    <Router>
+      <MainNavigation />
+      <main>
+        <Routes>
+          {/* react-router-dom version 6 no longer supports the use of components directly. Use an element to specify the component you route.
+  Route has to be a child of Routes  */}
+          <Route path="/" element={ <Users />} />
+          <Route path="/places/new" element={ <NewPlace /> } />
+        </Routes>
+      </main>
+      <Navigate to="/" />
+    </Router>
+  );
+};
+
+export default App;
